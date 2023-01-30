7c107fcae364f73f562bb9070c552cb86df540b6
Stefan Jacobs <stefanjacobsrips@outlook.com>
2023-01-30 13:49:19 +0100

    Day 01 Part 1 and 2

---
+++ AdventOfCode2022/Day01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace AdventOfCodeDay16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = File.ReadLines("input.txt").ToList();
+            List<int> calInts = new();
+            int countCal = 0;
+
+            foreach (var item in input)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    countCal += int.Parse(item);
+                }
+                else
+                {
+                    calInts.Add(countCal);
+                    countCal = 0;
+                }
+            }
+
+            calInts.Sort();
+
+            Console.WriteLine($"The answer of part 1 is: { calInts.Max() }");
+            Console.WriteLine($"The answer of part 2 is: { calInts.TakeLast(3).Sum() }");
+        }
+    }
+}
