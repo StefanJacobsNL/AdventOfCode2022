28ec89d43b00bff82763fbd2159741a94bbecc23
Stefan Jacobs <stefanjacobsrips@outlook.com>
2023-01-30 16:33:05 +0100

    Day 04 Part 01

---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCodeDay01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = File.ReadLines("input.txt").ToList();
+            
+            int answer1 = 0;
+            int answer2 = 0;
+
+            foreach (var textRow in input)
+            {
+                List<int> assignmentTexts = textRow.Split(',', '-').Select(int.Parse).ToList();
+
+                if (assignmentTexts[0] >= assignmentTexts[2] && assignmentTexts[1] <= assignmentTexts[3])
+                {
+                    answer1++;
+                }
+                else if (assignmentTexts[2] >= assignmentTexts[0] && assignmentTexts[3] <= assignmentTexts[1])
+                {
+                    answer1++;
+                }
+            }
+
+            Console.WriteLine($"The answer of part 1 is: { answer1 }");
+
+            
+
+            Console.WriteLine($"The answer of part 2 is: { answer2 }");
+        }
+    }
+}
