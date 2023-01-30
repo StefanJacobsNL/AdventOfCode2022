4fdafbde2d8b6a1f65d96d17f0168a6a31a978db
Stefan Jacobs <stefanjacobsrips@outlook.com>
2023-01-30 16:01:35 +0100

    Day 03 Part 2

--- AdventOfCode2022/Day03/Program.cs
+++ AdventOfCode2022/Day03/Program.cs
@@ -13,6 +13,7 @@ namespace AdventOfCodeDay01
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
             };
             int answer1 = 0;
+            int answer2 = 0;
             List<char> itemTypes = new();
 
             foreach (var item in input)
@@ -33,7 +34,28 @@ namespace AdventOfCodeDay01
             answer1 = itemTypes.Sum(x => abc.IndexOf(x) + 1);
 
             Console.WriteLine($"The answer of part 1 is: { answer1 }");
-            //Console.WriteLine($"The answer of part 2 is: { calInts.TakeLast(3).Sum() }");
+
+            itemTypes.Clear();
+            var groupInputBy3 = input.Select((x, i) => new { Index = i, Value = x }).
+                        GroupBy(x => x.Index / 3).ToList();
+
+            foreach (var item in groupInputBy3)
+            {
+                List<char> firstElf = item.First().Value.ToList();
+                List<char> secondElf = item.Skip(1).First().Value.ToList();
+                List<char> thirdElf = item.Last().Value.ToList();
+
+                var comparedItemType = firstElf.Where(x => secondElf.Contains(x) && thirdElf.Contains(x)).Distinct().ToList();
+
+                if (comparedItemType != null && comparedItemType.Any())
+                {
+                    itemTypes.AddRange(comparedItemType);
+                }
+            }
+
+            answer2 = itemTypes.Sum(x => abc.IndexOf(x) + 1);
+
+            Console.WriteLine($"The answer of part 2 is: { answer2 }");
         }
     }
 }
